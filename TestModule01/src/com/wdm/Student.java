9ef3739940eede766475e57812cabd40fb0c9050
wangdm <wangdm16@163.com>
2021-09-06 17:26:47 +0800

    第一次进行提交

---
+++ TestModule01/src/com/wdm/Student.java
@@ -0,0 +1,13 @@
+package com.wdm;
+
+import java.io.Serializable;
+
+/**
+ * @author wdm
+ * @ Date: 2021/9/6 17:20
+ * @ Description:
+ * @ version:1.0
+ */
+public class Student implements Serializable {
+
+}

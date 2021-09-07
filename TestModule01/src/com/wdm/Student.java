ddfa46f19faa6c0284fa196f843c9676bf2e5c43
wangdm <wangdm16@163.com>
2021-09-07 10:41:44 +0800

    加了main方法

--- TestModule01/src/com/wdm/Student.java
+++ TestModule01/src/com/wdm/Student.java
@@ -33,4 +33,8 @@ public class Student implements Serializable {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public static void main(String[] args) {
+        System.out.println("呵呵");
+    }
 }
